af97eb8c0737af6ba088bf7292ee70916259efdd
mizuki830 <mizuki.lovebaske.0830@ezweb.ne.jp>
2020-09-22 19:27:39 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-    
-  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password])
-  
+  end
+
   private
 
   def basic_auth
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, inclusion: {in: %w(@)}
+    validates :password, format: { with: /\A[a-z\d]{6}+\z/i}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/}
+    validates :birthday
+  end
 end
