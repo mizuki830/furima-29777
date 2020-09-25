e1dadbf06b803a9ee2d71b759e1a8abc4ba5180b
mizuki830 <mizuki.lovebaske.0830@ezweb.ne.jp>
2020-09-25 16:21:39 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shopping_change
+  belongs_to_active_hash :current_place
+  belongs_to_active_hash :send_day
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :explain, length: { maximum: 1000 }
+    validates :category, numericality: { other_than: 1 }
+    validates :condition, numericality: { other_than: 1 }
+    validates :shopping_change, numericality: { other_than: 1 }
+    validates :current_place, numericality: { other_than: 1 }
+    validates :send_day, numericality: { other_than: 1 }
+    validates :price, format: { with: /\A[0-9]+\z/ }, inclusion: {in: 300..9999999}
+  end
 end
